a69faf692cc3ebcad2c9b9442fd2ba48950c84f4
Gokul S Narayan <gokul.n@virtualveb.com>
2019-10-24 19:03:01 +0530

    changes

--- client/src/redux/auth/saga.js
+++ client/src/redux/auth/saga.js
@@ -1,8 +1,9 @@
 import io from 'socket.io-client';
-import { all, call, fork, put, takeEvery,takeLatest } from 'redux-saga/effects';
+import { all, call, fork, put, takeEvery, takeLatest } from 'redux-saga/effects';
 import { API_URL, SOCKET_URL } from '../../constants/defaultValues';
 import axios from 'axios';
 import { toastr } from 'react-redux-toastr';
+import swal from 'sweetalert';
 import {
     LOGIN_USER,
     LOGOUT_USER,
@@ -23,7 +24,7 @@ const headers = {
 }
 
 var socket = {};
-    
+
 const getSocket = (url) => {
     return io(url)
 };
@@ -38,26 +39,32 @@ function* loginWithEmailPassword({ payload }) {
             console.log("data========>>>", result)
             localStorage.setItem('token', result.data.token);
             try {
-            socket = getSocket(SOCKET_URL);
-            console.log("Socket")
-            yield put(setSocketData(socket))
-            let userData = yield axios.post(`${API_URL}/users/getUserDetails`, {}, {headers:{
-                'Content-Type': 'application/json',
-                'Authorization': result.data.token
-            }})
-            let user = userData.data.result;
-            yield put(setUserData(user));
-            // console.log("Userdata",user)
-            socket.emit('new user', user.user_name)
-        }
-        catch(err){
-            console.log("Error while getting user data",err)
-        }
+                socket = getSocket(SOCKET_URL);
+                console.log("Socket")
+                yield put(setSocketData(socket))
+                let userData = yield axios.post(`${API_URL}/users/getUserDetails`, {}, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': result.data.token
+                    }
+                })
+                let user = userData.data.result;
+                yield put(setUserData(user));
+                // console.log("Userdata",user)
+                socket.emit('new user', user.user_name)
+            }
+            catch (err) {
+                console.log("Error while getting user data", err)
+            }
             yield put(loginUserSuccess(result.data.token));
             history.push('/');
         } else {
             yield put(loginUserFailed())
-            toastr.error('Login', 'Invalid credentials');
+            // toastr.error('Login', 'Invalid credentials');
+            swal("Login Failed!", result.data.message, "error", {
+                buttons: false,
+                timer: 2000
+            });
         }
     }
     catch (err) {
@@ -65,38 +72,38 @@ function* loginWithEmailPassword({ payload }) {
     }
 }
 
-function* logout({payload}) {
-    console.log("payload at logout",payload)
-    const { history, user_name,id } = payload;
+function* logout({ payload }) {
+    console.log("payload at logout", payload)
+    const { history, user_name, id } = payload;
     yield localStorage.removeItem('token');
-    socket.emit('user left',id,user_name)
+    socket.emit('user left', id, user_name)
     history.push('/')
 }
 
-function* registerWithEmailPassword({payload}) {
-    const {user,history} = payload;
-    console.log("handle Register saga",history)
-    if(user){
-    try{
-    let result = yield axios.post(`${API_URL}/users/register`,{
-        user_name:user.user_name,
-        email:user.email,
-        password:user.password
-    });
-    console.log("result at register===>>",result);
-    if(result.data.status ==1){
-        yield put(registerUser("success"))
-        // history.push('/');
+function* registerWithEmailPassword({ payload }) {
+    const { user, history } = payload;
+    console.log("handle Register saga", history)
+    if (user) {
+        try {
+            let result = yield axios.post(`${API_URL}/users/register`, {
+                user_name: user.user_name,
+                email: user.email,
+                password: user.password
+            });
+            console.log("result at register===>>", result);
+            if (result.data.status == 1) {
+                yield put(registerUser("success"))
+                // history.push('/');
+            }
+        }
+        catch (err) {
+            console.log("Error at register saga", err)
+        }
     }
 }
-catch(err){
-    console.log("Error at register saga",err)
-}
-}
-}
 
 
-export function* watchRegisterUser(){
+export function* watchRegisterUser() {
     yield takeEvery(REGISTER_USER, registerWithEmailPassword);
 }
 
